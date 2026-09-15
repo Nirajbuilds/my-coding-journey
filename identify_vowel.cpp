e7468db06aeee8df94c2972257523c95a8144182
Nirajbuilds <subhashbarnwalniraj@gmail.com>
2026-09-15 20:09:44 +0530

    Add files via upload

---
+++ identify_vowel.cpp
@@ -0,0 +1,30 @@
+#include  <iostream>
+#include <string>
+using namespace std;
+int main() {
+char alphabet;
+cout << "Enter an alphabet : ";
+cin >> alphabet;
+switch(alphabet){
+    case 'a':
+        cout << "Vowel";
+        break;
+    case 'e':
+        cout << "Vowel"; 
+        break;
+    case 'i':
+        cout << "Vowel";
+        break;
+    case 'o':
+        cout << "Vowel";
+        break;
+    case 'u':
+        cout << "Vowel";
+        break;
+    default:
+        cout << "Consonant";
+        
+}
+
+    
+}
