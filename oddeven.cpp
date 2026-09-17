95077891702d52c0778065442d45ed79d6bb3e0a
Nirajbuilds <subhashbarnwalniraj@gmail.com>
2026-09-17 19:40:00 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,16 @@
+#include  <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+int n;
+cout << "enter your number" <<"\n";
+cin >> n;
+if(n%2 == 0){
+    cout <<"even" <<"\n";
+
+}
+else{
+    cout << "odd" <<"\n";
+}
+}
