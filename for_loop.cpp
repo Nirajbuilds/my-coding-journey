d85b0ebb1b9c6e7ae3d37fd387d48582062fa8b3
Nirajbuilds <subhashbarnwalniraj@gmail.com>
2026-09-17 19:41:29 +0530

    Add files via upload

---
+++ for_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>'
+#include <string>
+using namespace std;
+int main() {
+int n;
+cout << "Enter a number :";
+cin >> n;
+for(int i=1; i<=n; i++){
+
+cout << "hello !" <<"\n";
+
+
+}
+
+
+
+
+}
+
